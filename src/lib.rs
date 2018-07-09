36549b3c8e49016fe2c09336c66a8309436c62dd
Matthew Klein <mklein994@gmail.com>
2018-07-08 19:25:18 -0500

    Read lat/lon from .env, print sun info

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+extern crate chrono;
+extern crate dotenv;
+extern crate spa;
+
+mod error;
+
+use chrono::{DateTime, Utc};
+use error::DwError;
+
+type Result<T> = std::result::Result<T, DwError>;
+
+#[derive(Debug)]
+pub struct Config {
+    pub lat: f64,
+    pub lon: f64,
+    pub now: DateTime<Utc>,
+}
+
+pub fn run() -> Result<()> {
+    let config = get_config()?;
+    main(&config)
+}
+
+pub fn main(config: &Config) -> Result<()> {
+    let sun_position = spa::calc_solar_position(config.now, config.lat, config.lon)?;
+    println!("{:#?}", sun_position);
+    let sun_time = spa::calc_sunrise_and_set(config.now, config.lat, config.lon)?;
+    println!("{:#?}", sun_time);
+    Ok(())
+}
+
+pub fn get_config() -> Result<Config> {
+    dotenv::dotenv().ok();
+    let now = Utc::now();
+    let lat = std::env::var("WALLPAPER_LAT")?.parse::<f64>()?;
+    let lon = std::env::var("WALLPAPER_LON")?.parse::<f64>()?;
+    Ok(Config { now, lat, lon })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate dynamic_wallpaper;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = dynamic_wallpaper::run() {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
 }
