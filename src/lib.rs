348e8654a9389750e94c02cd948a92a13fc8cdb1
Matthew Klein <mklein994@gmail.com>
2018-07-21 14:14:43 -0500

    Get config from .env, and get sunrise and sunset

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,56 @@
 extern crate chrono;
 extern crate dotenv;
-extern crate spa;
 extern crate failure;
+extern crate spa;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Local, Utc};
 
 type Result<T> = std::result::Result<T, failure::Error>;
 
 #[derive(Debug)]
-pub struct Config {
+struct Config {
     pub lat: f64,
     pub lon: f64,
-    pub now: DateTime<Utc>,
+    pub now: DateTime<Local>,
+}
+
+impl Config {
+    fn new() -> Result<Self> {
+        let now = if let Ok(now_string) = std::env::var("WALLPAPER_NOW") {
+            DateTime::parse_from_rfc3339(&now_string)?.with_timezone(&Local)
+        } else {
+            Local::now()
+        };
+        let lat = std::env::var("WALLPAPER_LAT")?.parse::<f64>()?;
+        let lon = std::env::var("WALLPAPER_LON")?.parse::<f64>()?;
+        Ok(Config { now, lat, lon })
+    }
+
+    fn get_sunset_sunrise(&self) -> Result<(DateTime<Local>, DateTime<Local>)> {
+        let daylight = spa::calc_sunrise_and_set(self.now.with_timezone(&Utc), self.lat, self.lon)?;
+        match daylight {
+            spa::SunriseAndSet::Daylight(sr, ss) => {
+                Ok((sr.with_timezone(&Local), ss.with_timezone(&Local)))
+            }
+            _ => unimplemented!(),
+        }
+    }
 }
 
 pub fn run() -> Result<()> {
-    let config = get_config()?;
-    main(&config)
-}
-
-pub fn main(config: &Config) -> Result<()> {
-    let sun_position = spa::calc_solar_position(config.now, config.lat, config.lon)?;
-    println!("{:#?}", sun_position);
-    let sun_time = spa::calc_sunrise_and_set(config.now, config.lat, config.lon)?;
-    println!("{:#?}", sun_time);
-    Ok(())
-}
-
-pub fn get_config() -> Result<Config> {
     dotenv::dotenv().ok();
-    let now = Utc::now();
-    let lat = std::env::var("WALLPAPER_LAT")?.parse::<f64>()?;
-    let lon = std::env::var("WALLPAPER_LON")?.parse::<f64>()?;
-    Ok(Config { now, lat, lon })
+    env_logger::init();
+    info!("logging enabled");
+
+    let config = Config::new()?;
+    debug!("{:#?}", config);
+
+    let (sunset, sunrise) = config.get_sunset_sunrise()?;
+    info!("sunrise: {}", sunrise);
+    info!("sunset:  {}", sunset);
+
+    Ok(())
 }
