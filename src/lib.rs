30fed09b0dd98fbffd052cae9517b270a31cc7b4
Matthew Klein <mklein994@gmail.com>
2018-07-28 20:38:53 -0500

    It works!
    Everything is calculated in UTC, but the logs they're shown in local
    time to make debugging easier.
    
    The logic of this is inspired by the work done here[1], which I used as
    a launch point to start from.
    
    [1]: https://github.com/t1m0thyj/WinDynamicDesktop

--- src/lib.rs
+++ src/lib.rs
@@ -7,73 +7,97 @@ extern crate env_logger;
 extern crate failure;
 extern crate spa;
 
-use chrono::{DateTime, Local, Utc};
+use chrono::{DateTime, Duration, Local, Timelike, Utc};
 use failure::ResultExt;
 use std::fmt;
-
-const FORMAT: &str = "%F %l:%M:%S %P (%Z)";
 
 type Result<T> = std::result::Result<T, failure::Error>;
 
 pub fn run() -> Result<()> {
     init();
 
-    let config = Config::new()?;
+    let config = Config::new().with_context(|c| format!("Config: {}", c))?;
     info!("config set ✓");
     debug!("{:#?}", config);
 
-    let wallpaper = Wallpaper::new()?;
-    debug!("{:?}", wallpaper);
+    let wallpaper = Wallpaper::new().with_context(|c| format!("Wallpaper: {}", c))?;
+    debug!("{:#?}", wallpaper);
 
-    let sun = Sun::new(config.now, config.lat, config.lon)?;
-    debug!("{}", sun);
+    let sun = Sun::new(config.now, config.lat, config.lon).with_context(|c| format!("Sun: {}", c))?;
+    info!("{}", sun);
+    info!("now:    {}", config.now.with_timezone(&Local));
 
-    let time_period = get_time_period(config.now, sun.sunrise, sun.sunset);
+    let time_period = TimePeriod::new(config.now, sun.sunrise, sun.sunset);
     info!("{}", time_period);
 
-    let duration = match time_period {
-        TimePeriod::BeforeSunrise => {
-            let duration = sun.sunrise - sun.last_sunset;
-            info!(
-                "duration from last sunset to sunrise: {}",
-                format_duration(duration)
-            );
-            duration
-        }
-        TimePeriod::AfterSunset => {
-            let duration = sun.next_sunrise - sun.sunset;
-            info!(
-                "duration from sunset to next sunrise: {}",
-                format_duration(duration),
-            );
-            duration
-        }
-        TimePeriod::DayTime => {
-            let duration = sun.sunset - sun.sunrise;
-            info!(
-                "duration from sunrise to sunset: {}",
-                format_duration(duration),
-            );
-            duration
-        }
+    let (start, end) = match time_period {
+        TimePeriod::BeforeSunrise => (sun.last_sunset, sun.sunrise),
+        TimePeriod::DayTime => (sun.sunrise, sun.sunset),
+        TimePeriod::AfterSunset => (sun.sunset, sun.next_sunrise),
     };
+    debug!(
+        "start time: {} ({})",
+        start.with_timezone(&Local),
+        match time_period {
+            TimePeriod::DayTime => "sunrise",
+            TimePeriod::BeforeSunrise => "last sunset",
+            TimePeriod::AfterSunset => "sunset",
+        }
+    );
+    debug!("end time:   {}", end.with_timezone(&Local));
+
+    debug!(
+        "halfway:    {}",
+        start.with_timezone(&Local) + (end - start) / 2
+    );
+
+    let duration = end - start;
     debug!("duration: {}", format_duration(duration));
 
-    let count = match time_period {
+    let time_since_start = config.now - start;
+    debug!(
+        "time since start: {} ({}%)",
+        format_duration(time_since_start),
+        100 * time_since_start.num_nanoseconds().unwrap() / duration.num_nanoseconds().unwrap()
+    );
+
+    let image_count = match time_period {
         TimePeriod::DayTime => wallpaper.sunset - wallpaper.sunrise,
         _ => wallpaper.count - wallpaper.sunset + wallpaper.sunrise,
     };
-    debug!("image count: {}", count);
+    debug!("image count: {}", image_count);
 
-    // TODO: fix this. Everything else works.
-    let index = match time_period {
-        TimePeriod::DayTime => ((config.now - sun.sunrise).num_seconds()) % i64::from(count),
-        TimePeriod::BeforeSunrise => ((config.now - sun.last_sunset).num_seconds()) % i64::from(count),
-        TimePeriod::AfterSunset => ((config.now - sun.sunset).num_seconds()) % i64::from(count),
-    };
-    debug!("index: {:?}", index);
+    let timer_length = duration.num_nanoseconds().unwrap() / image_count;
+    debug!(
+        "timer length: {}",
+        format_duration(Duration::nanoseconds(timer_length))
+    );
+
+    let index = get_index(config.now, start, timer_length);
+
+    info!(
+        "index: {} of {} ({}%)",
+        index,
+        image_count,
+        index * 100 / image_count
+    );
+
+    println!(
+        "{}",
+        match time_period {
+            TimePeriod::DayTime => index + wallpaper.sunrise,
+            TimePeriod::AfterSunset => index / 2 + wallpaper.sunset,
+            TimePeriod::BeforeSunrise => index,
+        }
+    );
 
     Ok(())
+}
+
+fn get_index(now: DateTime<Utc>, start: DateTime<Utc>, timer_length: i64) -> i64 {
+    let elapsed_time = now - start;
+    debug!("elapsed time: {}", format_duration(elapsed_time));
+    elapsed_time.num_nanoseconds().unwrap() / timer_length
 }
 
 fn init() {
@@ -87,33 +111,9 @@ fn init() {
     info!("dotenv ok ✓");
 }
 
-fn get_time_period(
-    now: DateTime<Local>,
-    sunrise: DateTime<Local>,
-    sunset: DateTime<Local>,
-) -> TimePeriod {
-    use chrono::Timelike;
-    let now = now.num_seconds_from_midnight();
-    let sunrise = sunrise.num_seconds_from_midnight();
-    let sunset = sunset.num_seconds_from_midnight();
-    if now > sunset {
-        TimePeriod::AfterSunset
-    } else if now < sunrise {
-        TimePeriod::BeforeSunrise
-    } else {
-        TimePeriod::DayTime
-    }
-}
-
-fn format_duration(d: chrono::Duration) -> String {
-    use chrono_humanize::{Accuracy, HumanTime, Tense};
-
-    HumanTime::from(d).to_text_en(Accuracy::Precise, Tense::Present)
-}
-
 #[derive(Debug)]
 struct Config {
-    now: DateTime<Local>,
+    now: DateTime<Utc>,
     lat: f64,
     lon: f64,
 }
@@ -123,19 +123,22 @@ impl Config {
         use std::env;
 
         let now = if let Ok(n) = env::var("WALLPAPER_NOW") {
-            DateTime::parse_from_rfc3339(&n)?.with_timezone(&Local)
+            warn!("Using WALLPAPER_NOW as current time");
+            DateTime::parse_from_rfc3339(&n)
+                .with_context(|c| format!("WALLPAPER_NOW: {}", c))?
+                .with_timezone(&Utc)
         } else {
-            Local::now()
+            Utc::now()
         };
 
         let lat = env::var("WALLPAPER_LAT")
-            .context("WALLPAPER_LAT is not set.")?
-            .parse::<f64>()?;
+            .with_context(|c| format!("WALLPAPER_LAT: {}", c))?
+            .parse::<f64>()
+            .with_context(|c| format!("WALLPAPER_LAT: {}", c))?;
         let lon = env::var("WALLPAPER_LON")
-            .context("WALLPAPER_LON is not set.")?
-            .parse::<f64>()?;
-
-        debug!("now :: {}", now.format(FORMAT));
+            .with_context(|c| format!("WALLPAPER_LON: {}", c))?
+            .parse::<f64>()
+            .with_context(|c| format!("WALLPAPER_LON: {}", c))?;
 
         Ok(Self { now, lat, lon })
     }
@@ -143,9 +146,9 @@ impl Config {
 
 #[derive(Debug)]
 struct Wallpaper {
-    count: u8,
-    sunrise: u8,
-    sunset: u8,
+    count: i64,
+    sunrise: i64,
+    sunset: i64,
 }
 
 impl Wallpaper {
@@ -153,60 +156,74 @@ impl Wallpaper {
         use std::env;
 
         let count = env::var("WALLPAPER_COUNT")
-            .context("WALLPAPER_COUNT not set.")?
+            .with_context(|c| format!("WALLPAPER_COUNT: {}", c))?
             .parse::<u8>()?;
         let sunrise = env::var("WALLPAPER_SUNRISE")
-            .context("WALLPAPER_SUNRISE not set.")?
+            .with_context(|c| format!("WALLPAPER_SUNRISE: {} ", c))?
             .parse::<u8>()?;
         let sunset = env::var("WALLPAPER_SUNSET")
-            .context("WALLPAPER_SUNSET not set.")?
-            .parse::<u8>()?;
+            .with_context(|c| format!("WALLPAPER_SUNSET: {}", c))?
+            .parse::<u8>()
+            .with_context(|c| format!("WALLPAPER_SUNSET: {}", c))?;
 
         Ok(Self {
-            count,
-            sunrise,
-            sunset,
+            count: i64::from(count),
+            sunrise: i64::from(sunrise),
+            sunset: i64::from(sunset),
         })
     }
 }
 
 #[derive(Debug)]
 struct Sun {
-    last_sunset: DateTime<Local>,
-    sunrise: DateTime<Local>,
-    sunset: DateTime<Local>,
-    next_sunrise: DateTime<Local>,
+    last_sunset: DateTime<Utc>,
+    sunrise: DateTime<Utc>,
+    sunset: DateTime<Utc>,
+    next_sunrise: DateTime<Utc>,
 }
 
 impl Sun {
-    fn new(now: DateTime<Local>, lat: f64, lon: f64) -> Result<Self> {
-        use chrono::Duration;
+    fn new(now: DateTime<Utc>, lat: f64, lon: f64) -> Result<Self> {
+        use spa::SunriseAndSet;
 
-        let (sunrise, sunset) = match spa::calc_sunrise_and_set(now.with_timezone(&Utc), lat, lon)?
-        {
-            spa::SunriseAndSet::Daylight(sunrise, sunset) => {
-                (sunrise.with_timezone(&Local), sunset.with_timezone(&Local))
-            }
+        //let utc_now = Utc::now();
+        let utc_now = now
+            .with_timezone(&Local)
+            .date()
+            .and_hms(12, 0, 0)
+            .with_timezone(&Utc);
+
+        debug!("now:          {}", now.with_timezone(&Local));
+        debug!("UTC now:      {}", utc_now.with_timezone(&Local));
+
+        debug_assert!(Utc::today() - Duration::days(1) <= now.date());
+        debug_assert!(now.date() <= Utc::today() + Duration::days(1));
+
+        let (sunrise, sunset) = match spa::calc_sunrise_and_set(utc_now, lat, lon)? {
+            SunriseAndSet::Daylight(sunrise, sunset) => (sunrise, sunset),
             _ => unimplemented!(),
         };
+        debug!("sunrise:      {}", sunrise.with_timezone(&Local));
+        debug!("sunset:       {}", sunset.with_timezone(&Local));
 
-        let last_sunset = match spa::calc_sunrise_and_set(
-            (now.date().and_hms(12, 0, 0) - Duration::hours(24)).with_timezone(&Utc),
-            lat,
-            lon,
-        )? {
-            spa::SunriseAndSet::Daylight(_, sunset) => sunset.with_timezone(&Local),
+        let last_sunset = match spa::calc_sunrise_and_set(utc_now - Duration::days(1), lat, lon)? {
+            SunriseAndSet::Daylight(_, sunset) => sunset,
             _ => unimplemented!(),
         };
+        debug!("last sunset:  {}", last_sunset.with_timezone(&Local));
 
-        let next_sunrise = match spa::calc_sunrise_and_set(
-            (now.date().and_hms(12, 0, 0) + Duration::hours(24)).with_timezone(&Utc),
-            lat,
-            lon,
-        )? {
-            spa::SunriseAndSet::Daylight(sunrise, _) => sunrise.with_timezone(&Local),
+        let next_sunrise = match spa::calc_sunrise_and_set(utc_now + Duration::days(1), lat, lon)? {
+            spa::SunriseAndSet::Daylight(sunrise, _) => sunrise,
             _ => unimplemented!(),
         };
+        debug!("next sunrise: {}", next_sunrise.with_timezone(&Local));
+
+        debug_assert!(last_sunset < sunrise);
+        debug_assert!(sunrise < sunset);
+        debug_assert!(sunset < next_sunrise);
+
+        debug_assert!(last_sunset.with_nanosecond(0).unwrap() <= now.with_nanosecond(0).unwrap());
+        debug_assert!(now.with_nanosecond(0).unwrap() <= next_sunrise.with_nanosecond(0).unwrap());
 
         Ok(Self {
             last_sunset,
@@ -222,15 +239,15 @@ impl fmt::Display for Sun {
         write!(
             f,
             "Sun:\n{}\n{:<13} {}\n{:<13} {}\n{:<13} {}\n{:<13} {}",
-            "-".repeat(45),
+            "-".repeat(50),
             "last sunset:",
-            self.last_sunset.format(FORMAT),
+            self.last_sunset.with_timezone(&Local),
             "sunrise:",
-            self.sunrise.format(FORMAT),
+            self.sunrise.with_timezone(&Local),
             "sunset:",
-            self.sunset.format(FORMAT),
+            self.sunset.with_timezone(&Local),
             "next sunrise:",
-            self.next_sunrise.format(FORMAT)
+            self.next_sunrise.with_timezone(&Local)
         )
     }
 }
@@ -250,4 +267,21 @@ impl fmt::Display for TimePeriod {
             TimePeriod::DayTime => write!(f, "\u{1f3d9} Daytime"),
         }
     }
+}
+
+impl TimePeriod {
+    fn new(now: DateTime<Utc>, sunrise: DateTime<Utc>, sunset: DateTime<Utc>) -> Self {
+        if now <= sunrise {
+            TimePeriod::BeforeSunrise
+        } else if now <= sunset {
+            TimePeriod::DayTime
+        } else {
+            TimePeriod::AfterSunset
+        }
+    }
+}
+
+fn format_duration(duration: Duration) -> String {
+    use chrono_humanize::{Accuracy, HumanTime, Tense};
+    HumanTime::from(duration).to_text_en(Accuracy::Precise, Tense::Present)
 }
