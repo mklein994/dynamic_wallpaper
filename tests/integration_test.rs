bd3f3c6aacd240a363ea475e713d1576d65d187d
Matthew Klein <mklein994@gmail.com>
2025-03-02 14:48:19 -0600

    Mark set_var use (in integration test) as unsafe in preparation for 2024 edition

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,9 +1,12 @@
 #[test]
 fn run_smoke_test() {
-    std::env::set_var(
-        "DYNAMIC_WALLPAPER_CONFIG",
-        std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/test_config.toml"),
-    );
+    // TODO: Audit that the environment access only happens in single-threaded code.
+    unsafe {
+        std::env::set_var(
+            "DYNAMIC_WALLPAPER_CONFIG",
+            std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/test_config.toml"),
+        );
+    };
     let index = dynamic_wallpaper::run().unwrap();
 
     assert_eq!(3, index);
